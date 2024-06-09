e2faebd47d1aaea52539c6c7a6074111790867b7
oddyamill <43600905+oddyamill@users.noreply.github.com>
2024-06-09 21:36:19 +0700

    sri

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { sri } from 'vite-plugin-sri3'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
+    sri(),
   ],
   resolve: {
     alias: {
