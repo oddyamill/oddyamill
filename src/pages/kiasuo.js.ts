3cedbf2fbee240fe66a3fdeaebeb7c694c5cf38c
oddyamill <43600905+oddyamill@users.noreply.github.com>
2024-11-05 14:33:28 +0700

    feat warp status

--- src/pages/kiasuo.js.ts
+++ src/pages/kiasuo.js.ts
@@ -1,5 +1,7 @@
 export async function GET() {
-  return fetch('https://raw.githubusercontent.com/oddyamill/kiasuo/refs/heads/master/scripts/registration.js')
+  return fetch(
+    'https://raw.githubusercontent.com/oddyamill/kiasuo/refs/heads/master/scripts/registration.js',
+  )
 }
 
 export const prerender = true
